6f01898889090e52002cc007cd057b011fe3ae31
Hugo Bena <heno.drawing@gmail.com>
2026-02-04 16:46:36 +0100

    DELETE(CorsMiddleware) fichier inutile ou renommage

---
+++ mobile/lib/core/theme/theme.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter/material.dart';
+// import 'app_colors.dart';
+
+// class AppTheme {
+//   static ThemeData get lightTheme {
+//     return ThemeData(
+//       primaryColor: AppColors.darkGrey,
+//       scaffoldBackgroundColor: AppColors.background,
+//       colorScheme: ColorScheme.fromSeed(
+//         seedColor: AppColors.primary,
+//         primary: AppColors.primary,
+//       ),
+//       useMaterial3: true,
+//       appBarTheme: const AppBarTheme(
+//         backgroundColor: AppColors.lightGrey,
+//         elevation: 0,
+//       ),
+//     );
+//   }
+// }
